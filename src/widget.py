f74335802c5dd16a95bda4f14d27434225001262
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-10-24 16:19:00 +0300

    Add the 'format_the_date' function.

--- src/widget.py
+++ src/widget.py
@@ -11,3 +11,10 @@ def mask_user_data(card_details: str, score_details: str) -> str:
         f'{" ".join(data_card[:-1])} {masks.mask_the_card(data_card[-1])}'
         f'\n{" ".join(data_score[:-1])} {masks.mask_the_score(data_score[-1])}'
     )
+
+
+def format_the_date(data: str) -> str:
+    """Возвращает удобный для чтения формат даты."""
+    new_data = data.split("-")
+
+    return f"{new_data[2][:2]}.{new_data[1]}.{new_data[0]}"
