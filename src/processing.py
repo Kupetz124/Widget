1d3900f8e8f3a67dec05fced304508a878e80323
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-10-29 13:57:40 +0300

    Add the 'sort_by_date' function.

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,15 @@ def select_by_key(data: list[dict], key: str = "EXECUTED") -> list[dict]:
     :return:Список словарей, содержащих второй аргумент функции по ключу "state".
     """
     return [item for item in data if item["state"] == key]
+
+
+def sort_by_date(data: list[dict], reversal: bool = True) -> list[dict]:
+    """
+    Сортирует список словарей по дате (ключ "date").
+    Если reversal == True, то идёт сортировка по убыванию.
+
+    :param data:Список словарей с данными.
+    :param reversal:Тип (bool), по умолчанию "True" для направления сортировки.
+    :return:Список словарей, отсортированный по дате (ключ "date").
+    """
+    return sorted(data, key=lambda x: x["date"], reverse=reversal)
