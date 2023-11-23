13cd11af87cd21d4018e488a8477fffe80e77606
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-11-23 20:37:59 +0300

    Add logging to file

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
+import logging
 from typing import Any
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(f"{__name__}.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(funcName)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.INFO)
 
 
 def get_transactions_data(file_path: str) -> Any:
@@ -13,10 +22,13 @@ def get_transactions_data(file_path: str) -> Any:
     """
     try:
         with open(file_path, "r", encoding="utf-8") as file:
+            logger.info("'json' файл прочитан.")
             return json.load(file)
     except json.JSONDecodeError:
+        logger.info("Ошибка 'json.JSONDecodeError'")
         return []
     except FileNotFoundError:
+        logger.info("Ошибка 'FileNotFoundError'")
         return []
 
 
@@ -27,6 +39,8 @@ def get_sum_transaction(data: dict) -> float | str:
     :return: сумма транзакции или сообщение об ошибке
     """
     if data["operationAmount"]["currency"]["code"] == "RUB":
+        logger.info("Получена транзакция в 'RUB'.")
         return float(data["operationAmount"]["amount"])
     else:
+        logger.info("Валюта транзакции не в рублях.")
         return "ValueError: Транзакция выполнена не в рублях. Укажите транзакцию в рублях."
