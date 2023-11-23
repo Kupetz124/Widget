fc33cc7b553dc4bfc4f43bf74fe23c03e3e718db
__Vitaliy__ <akkauntmoy124@gmail.com>
2023-11-23 20:37:44 +0300

    Add logging to file

--- src/masks.py
+++ src/masks.py
@@ -1,5 +1,15 @@
 # модуль с функциями для маскировки номеров карт и счетов
 
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(f"{__name__}.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(funcName)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.INFO)
+
 
 def mask_the_card(number_card: str) -> str:
     """
@@ -7,6 +17,7 @@ def mask_the_card(number_card: str) -> str:
     :param number_card: Строка с номером карты.
     :return: Строка с замаскированным номером карты.
     """
+    logger.info("Номер карты замаскирован.")
     return f"{number_card[:4]} {number_card[4:6]}** **** {number_card[12:]}"
 
 
@@ -16,4 +27,5 @@ def mask_the_score(number_score: str) -> str:
     :param number_score: Строка с номером счёта.
     :return:Строка с замаскированным номером счёта.
     """
+    logger.info("Номер счёта замаскирован.")
     return "**" + number_score[-4:]
